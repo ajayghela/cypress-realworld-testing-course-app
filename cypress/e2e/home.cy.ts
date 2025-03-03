cfc0cae5acc801795189105a2de5c2969e63fb51
Ajay Ghela <100951275+ajayghela@users.noreply.github.com>
2025-03-03 11:44:56 +0000

    h1 -> attribute

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,6 @@
 describe('Home page', () => {
   it('passes', () => {
     cy.visit('http://localhost:3002')
-    cy.get("h1").contains("Testing Next.js Applications with Cypress")
+    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
   })
 })

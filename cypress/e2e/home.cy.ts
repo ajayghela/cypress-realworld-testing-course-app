71b71804c29bf68634d0cb34bdab666280c9b05a
Ajay Ghela <100951275+ajayghela@users.noreply.github.com>
2025-03-03 13:29:33 +0000

    it.only()

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,10 @@
 describe('Home page', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3002')
+    cy.visit('http://localhost:3001')
     cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+  })
+  it.only("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3001")
+    cy.get("dt")
   })
 })

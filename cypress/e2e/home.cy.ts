6f77d21b169f7ae25cc42aec2c6ef9c6684ede88
Ajay Ghela <100951275+ajayghela@users.noreply.github.com>
2025-03-06 14:54:59 +0000

    getByData("hero-heading") <- refactor

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,7 +5,7 @@ describe('Home page', () => {
 
   it('passes', () => {
     
-    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+    cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
   })
   it.only("the features on the homepage are correct", () => {
     cy.get("dt").eq(0).contains("4 Courses")

2616f296ab7eee8fbf8be7ba5865680a6d55b673
Ajay Ghela <100951275+ajayghela@users.noreply.github.com>
2025-03-10 14:09:58 +0000

    Updated subscribeTest: Users can successfully subscribe

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -2,6 +2,10 @@ describe("Newsletter Subscribe Form", () => {
     beforeEach(()=>{
         cy.visit("http://localhost:3001")
     })
-    it("allows users to subscribe to the email list", () => {})
-        cy.getByData("email-input")
+    it("allows users to subscribe to the email list", () => {
+        cy.getByData("email-input").type("tom@aol.com")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message")
+        cy.getByData("success-message").should("exist").contains("tom@aol.com")
+    })
 })
